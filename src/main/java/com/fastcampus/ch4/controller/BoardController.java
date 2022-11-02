208487d9f02ab30e82d88fb631cf6775d7737138
EunjoungAhn <aej9300@gmail.com>
2022-11-02 22:20:35 +0900

    게시판 검색 서비스 및 태그 저장처리 text로 <c:out>으로 처리

--- src/main/java/com/fastcampus/ch4/controller/BoardController.java
+++ src/main/java/com/fastcampus/ch4/controller/BoardController.java
@@ -19,8 +19,7 @@ public class BoardController {
     BoardService boardService;
 
     @PostMapping("/modify")
-    public String modify(BoardDto boardDto, Integer page, Integer pageSize, RedirectAttributes rattr, Model m, HttpSession session) {
-
+    public String modify(BoardDto boardDto, SearchCondition sc, RedirectAttributes rattr, Model m, HttpSession session) {
         String writer = (String)session.getAttribute("id");
         boardDto.setWriter(writer);
 
@@ -28,17 +27,13 @@ public class BoardController {
             if (boardService.modify(boardDto)!= 1)
                 throw new Exception("Modify failed.");
 
-            rattr.addAttribute("page", page);
-            rattr.addAttribute("pageSize", pageSize);
             rattr.addFlashAttribute("msg", "MOD_OK");
-            return "redirect:/board/list";
+            return "redirect:/board/list"+sc.getQueryString();
         } catch (Exception e) {
             e.printStackTrace();
             m.addAttribute(boardDto);
-            m.addAttribute("page", page);
-            m.addAttribute("pageSize", pageSize);
             m.addAttribute("msg", "MOD_ERR");
-            return "board"; // 등록하려던 내용을 보여줘야 함.
+            return "board";
         }
     }
 
@@ -69,8 +64,22 @@ public class BoardController {
         }
     }
 
+    @GetMapping("/read")
+    public String read(Integer bno, SearchCondition sc, RedirectAttributes rattr, Model m) {
+        try {
+            BoardDto boardDto = boardService.read(bno);
+            m.addAttribute(boardDto);
+        } catch (Exception e) {
+            e.printStackTrace();
+            rattr.addFlashAttribute("msg", "READ_ERR");
+            return "redirect:/board/list"+sc.getQueryString();
+        }
+
+        return "board";
+    }
+
     @PostMapping("/remove")
-    public String remove(Integer bno, Integer page, Integer pageSize, RedirectAttributes rattr, HttpSession session) {
+    public String remove(Integer bno, SearchCondition sc, RedirectAttributes rattr, HttpSession session) {
         String writer = (String)session.getAttribute("id");
         String msg = "DEL_OK";
 
@@ -82,52 +91,22 @@ public class BoardController {
             msg = "DEL_ERR";
         }
 
-        rattr.addAttribute("page", page);
-        rattr.addAttribute("pageSize", pageSize);
         rattr.addFlashAttribute("msg", msg);
-        return "redirect:/board/list";
-    }
-    @GetMapping("/read")
-    public String read(Integer bno, Integer page, Integer pageSize, RedirectAttributes rattr, Model m) {
-        try {
-            BoardDto boardDto = boardService.read(bno);
-            //m.addAttribute("boardDto", boardDto); //아래 문장과 동일
-            m.addAttribute(boardDto);
-            m.addAttribute("page", page);
-            m.addAttribute("pageSize", pageSize);
-        } catch (Exception e) {
-            e.printStackTrace();
-            rattr.addAttribute("page", page);
-            rattr.addAttribute("pageSize", pageSize);
-            rattr.addFlashAttribute("msg", "READ_ERR");
-            return "redirect:/board/list";
-        }
-
-        return "board";
+        return "redirect:/board/list"+sc.getQueryString();
     }
 
     @GetMapping("/list")
-    public String list(@RequestParam(defaultValue ="1") Integer page,
-                       @RequestParam(defaultValue = "10") Integer pageSize,Model m, HttpServletRequest request) {
+    public String list(Model m, SearchCondition sc, HttpServletRequest request) {
         if(!loginCheck(request))
-            return "redirect:/login/login?toURL="+request.getRequestURL(); // 로그인을 안했으면 로그인 화면으로 이동
+            return "redirect:/login/login?toURL="+request.getRequestURL();  // 로그인을 안했으면 로그인 화면으로 이동
 
         try {
-            int totalCnt = boardService.getCount();
+            int totalCnt = boardService.getSearchResultCnt(sc);
             m.addAttribute("totalCnt", totalCnt);
 
-            PageHandler pageHandler = new PageHandler(totalCnt, page, pageSize);
+            PageHandler pageHandler = new PageHandler(totalCnt, sc);
 
-            if(page < 0 || page > pageHandler.getTotalPage())
-                page = 1;
-            if(pageSize < 0 || pageSize > 50)
-                pageSize = 10;
-
-            Map map = new HashMap();
-            map.put("offset", (page-1)*pageSize);
-            map.put("pageSize", pageSize);
-
-            List<BoardDto> list = boardService.getPage(map);
+            List<BoardDto> list = boardService.getSearchResultPage(sc);
             m.addAttribute("list", list);
             m.addAttribute("ph", pageHandler);
 
@@ -143,9 +122,9 @@ public class BoardController {
     }
 
     private boolean loginCheck(HttpServletRequest request) {
-        // 1. 세션을 얻어서
-        HttpSession session = request.getSession();
+        // 1. 세션을 얻어서(false는 session이 없어도 새로 생성하지 않는다. 반환값 null)
+        HttpSession session = request.getSession(false);
         // 2. 세션에 id가 있는지 확인, 있으면 true를 반환
-        return session.getAttribute("id")!=null;
+        return session!=null && session.getAttribute("id")!=null;
     }
 }
--- src/main/java/com/fastcampus/ch4/domain/PageHandler.java
+++ src/main/java/com/fastcampus/ch4/domain/PageHandler.java
@@ -1,9 +1,14 @@
 package com.fastcampus.ch4.domain;
 
+import org.springframework.web.util.*;
+
 public class PageHandler {
-    private int naviSize = 10; // page navigation size
-    private int page; // 현재 페이지
-    private int pageSize; // 한 페이지의 크기
+    private SearchCondition sc;
+    //    private int pageSize = 10; // 한 페이지당 게시물 갯수
+//    private int page; // 현재 페이지
+//    private String  option;
+//    private String  keyword;
+    public  final int NAV_SIZE = 10; // page navigation size
     private int totalCnt; // 게시물의 총 갯수
     private int totalPage; // 전체 페이지의 갯수
     private int beginPage; // 화면에 보여줄 첫 페이지
@@ -11,28 +16,60 @@ public class PageHandler {
     private boolean showNext = false; // 이후를 보여줄지의 여부. endPage==totalPage이면, showNext는 false
     private boolean showPrev = false; // 이전을 보여줄지의 여부. beginPage==1이 아니면 showPrev는 false
 
-    public int getNaviSize() {
-        return naviSize;
+    public PageHandler(int totalCnt, Integer page) {
+        this(totalCnt, new SearchCondition(page, 10));
     }
 
-    public void setNaviSize(int naviSize) {
-        this.naviSize = naviSize;
+    public PageHandler(int totalCnt, Integer page, Integer pageSize) {
+        this(totalCnt, new SearchCondition(page, pageSize));
     }
 
-    public int getPage() {
-        return page;
+    public PageHandler(int totalCnt, SearchCondition sc) {
+        this.totalCnt = totalCnt;
+        this.sc = sc;
+
+        doPaging(totalCnt, sc);
     }
 
-    public void setPage(int page) {
-        this.page = page;
+    private void doPaging(int totalCnt, SearchCondition sc) {
+        this.totalPage = totalCnt / sc.getPageSize() + (totalCnt % sc.getPageSize()==0? 0:1);
+        this.sc.setPage(Math.min(sc.getPage(), totalPage));  // page가 totalPage보다 크지 않게
+        this.beginPage = (this.sc.getPage() -1) / NAV_SIZE * NAV_SIZE + 1; // 11 -> 11, 10 -> 1, 15->11. 따로 떼어내서 테스트
+        this.endPage = Math.min(beginPage + NAV_SIZE - 1, totalPage);
+        this.showPrev = beginPage!=1;
+        this.showNext = endPage!=totalPage;
     }
 
-    public int getPageSize() {
-        return pageSize;
+    public String getQueryString() {
+        return getQueryString(this.sc.getPage());
     }
 
-    public void setPageSize(int pageSize) {
-        this.pageSize = pageSize;
+    public String getQueryString(Integer page) {
+        // ?page=10&pageSize=10&option=A&keyword=title
+        return UriComponentsBuilder.newInstance()
+                .queryParam("page",     page)
+                .queryParam("pageSize", sc.getPageSize())
+                .queryParam("option",   sc.getOption())
+                .queryParam("keyword",  sc.getKeyword())
+                .build().toString();
+    }
+
+    void print() {
+        System.out.println("page="+ sc.getPage());
+        System.out.print(showPrev? "PREV " : "");
+
+        for(int i=beginPage;i<=endPage;i++) {
+            System.out.print(i+" ");
+        }
+        System.out.println(showNext? " NEXT" : "");
+    }
+
+    public SearchCondition getSc() {
+        return sc;
+    }
+
+    public void setSc(SearchCondition sc) {
+        this.sc = sc;
     }
 
     public int getTotalCnt() {
@@ -43,12 +80,12 @@ public class PageHandler {
         this.totalCnt = totalCnt;
     }
 
-    public int getTotalPage() {
-        return totalPage;
+    public boolean isShowNext() {
+        return showNext;
     }
 
-    public void setTotalPage(int totalPage) {
-        this.totalPage = totalPage;
+    public void setShowNext(boolean showNext) {
+        this.showNext = showNext;
     }
 
     public int getBeginPage() {
@@ -59,20 +96,24 @@ public class PageHandler {
         this.beginPage = beginPage;
     }
 
+    public int getNAV_SIZE() {
+        return NAV_SIZE;
+    }
+
+    public int getTotalPage() {
+        return totalPage;
+    }
+
+    public void setTotalPage(int totalPage) {
+        this.totalPage = totalPage;
+    }
+
     public int getEndPage() {
         return endPage;
     }
 
     public void setEndPage(int endPage) {
         this.endPage = endPage;
-    }
-
-    public boolean isShowNext() {
-        return showNext;
-    }
-
-    public void setShowNext(boolean showNext) {
-        this.showNext = showNext;
     }
 
     public boolean isShowPrev() {
@@ -83,43 +124,17 @@ public class PageHandler {
         this.showPrev = showPrev;
     }
 
-    public PageHandler(int totalCnt, int page){
-            this(totalCnt, page, 10);
-        }
-
-        public PageHandler(int totalCnt, int page, int pageSize) {
-            this.totalCnt = totalCnt;
-            this.page = page;
-            this.pageSize = pageSize;
-
-
-            this.totalPage = (int)Math.ceil(totalCnt / (double)pageSize);// 전체 페이지에서 마지막 페이징 구간에서 반올림이 안됨으로 더블로 처리
-            this.beginPage = (page-1) / naviSize * naviSize + 1; // 11 -> 11, 10 -> 1, 15->11. 따로 떼어내서 테스트
-            this.endPage = Math.min(beginPage + naviSize - 1, totalPage); // 둘중 작은 값이 담긴다.
-            this.showPrev = beginPage!=1;
-            this.showNext = endPage!=totalPage;
-        }
-
-        void print() {
-            System.out.println("page="+ page);
-            System.out.print(showPrev? "PREV " : "");
-
-            for(int i=beginPage;i<=endPage;i++) {
-                System.out.print(i+" ");
-            }
-            System.out.println(showNext? " NEXT" : "");
-        }
-
-        @Override
-        public String toString() {
-            return "PageHandler{" +
-                    ", totalCnt=" + totalCnt +
-                    ", showNext=" + showNext +
-                    ", beginPage=" + beginPage +
-                    ", naviSize=" + naviSize +
-                    ", totalPage=" + totalPage +
-                    ", endPage=" + endPage +
-                    ", showPrev=" + showPrev +
-                    '}';
-        }
+    @Override
+    public String toString() {
+        return "PageHandler{" +
+                "sc=" + sc +
+                ", totalCnt=" + totalCnt +
+                ", showNext=" + showNext +
+                ", beginPage=" + beginPage +
+                ", NAV_SIZE=" + NAV_SIZE +
+                ", totalPage=" + totalPage +
+                ", endPage=" + endPage +
+                ", showPrev=" + showPrev +
+                '}';
+    }
 }
